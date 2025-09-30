69b44c07bf03443901c370b13c689fd8930195db
sions5801 <ss05264491@priestley.ac.uk>
2025-09-30 20:45:46 +0100

    begin implementing database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,22 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace CertiPurge.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Certificate> Certificates { get; set; }
+}
+
+public class Certificate
+{
+    public int Id { get; set; }
+    public string CandidateNumber { get; set; }
+    public string CentreNumber { get; set; }
+    public string ULN { get; set; }
+    public string Name { get; set; }
+    public string Course { get; set; }
+    public string Grade { get; set; }
+    public string DateIssued { get; set; }
+    public string DestructionDate { get; set; }
+    public bool Destroyed { get; set; }
 }
