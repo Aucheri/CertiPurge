9c69664b1587add117c860d575d6732bd5d6752c
sions5801 <ss05264491@priestley.ac.uk>
2025-10-06 10:36:42 +0100

    add exam board and rename date issued

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,12 +12,13 @@ public class ApplicationDbContext : DbContext
 public class Certificate
 {
     public int Id { get; set; }
+    public required string ExamBoard { get; set; }
     public required string CandidateNumber { get; set; }
     public required string CentreNumber { get; set; }
     public required string ULN { get; set; }
     public required string Name { get; set; }
     public required string Course { get; set; }
     public required string Grade { get; set; }
-    public string? DateIssued { get; set; }
+    public string? AwardingDate { get; set; }
     public DateOnly DestructionDate { get; set; }
 }
