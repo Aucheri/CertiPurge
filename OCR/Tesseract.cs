e6134d83c76977367c7fcebfb14ee185863fd080
liamg2810 <lgardn3r@gmail.com>
2025-10-21 10:17:39 +0100

    All 4 certificate types on there now

--- OCR/Tesseract.cs
+++ OCR/Tesseract.cs
@@ -93,7 +93,7 @@ class Readers
 			previousLine = content;
 		}
 	}
-	
+
 	public static void ReadBTEC(string text, out string name, out string course, out string grade, out string centrenum, out string candidate)
 	{
 		name = string.Empty;
@@ -146,6 +146,122 @@ class Readers
 
 				centrenum = parts[0].Trim();
 				candidate = parts[2].Split(":")[0].Trim();
+			}
+
+			previousLine = content;
+		}
+	}
+
+	public static void ReadEDUQAS(string text, out string name, out string course, out string grade, out string centrenum, out string candidate)
+	{
+		name = string.Empty;
+		course = string.Empty;
+		grade = string.Empty;
+		centrenum = string.Empty;
+		candidate = string.Empty;
+
+		string previousLine = string.Empty;
+
+		if (string.IsNullOrWhiteSpace(text))
+		{
+			return;
+		}
+
+		foreach (var line in text.Split("\n"))
+		{
+			if (string.IsNullOrWhiteSpace(line))
+			{
+				continue;
+			}
+
+
+			var content = GetContentFromWordString(line);
+
+			if (string.IsNullOrWhiteSpace(content))
+			{
+				continue;
+			}
+
+			if (line.Contains("certify that") && string.IsNullOrWhiteSpace(name))
+			{
+				var parts = content.Split("certify that");
+
+				name = parts[1].Trim();
+
+				Console.WriteLine($"Extracted name: {name}");
+			}
+			else if (previousLine.Contains("ADVANCED LEVEL") && string.IsNullOrWhiteSpace(course))
+			{
+				course = content.Split("  ")[0].Trim();
+
+				grade = content.Split("  ")[1].Trim();
+			}
+
+			previousLine = content;
+		}
+	}
+	
+	public static void ReadOCR(string text, out string name, out string course, out string grade, out string centrenum, out string candidate)
+	{
+		name = string.Empty;
+		course = string.Empty;
+		grade = string.Empty;
+		centrenum = string.Empty;
+		candidate = string.Empty;
+
+		string previousLine = string.Empty;
+		bool nameOnNext = false;
+
+		if (string.IsNullOrWhiteSpace(text))
+		{
+			return;
+		}
+
+		foreach (var line in text.Split("\n"))
+		{
+			Console.WriteLine(line);
+
+			if (string.IsNullOrWhiteSpace(line))
+			{
+				continue;
+			}
+
+
+			var content = GetContentFromWordString(line);
+
+			if (string.IsNullOrWhiteSpace(content))
+			{
+				continue;
+			}
+
+			if (previousLine.Contains("subject(s) shown") && string.IsNullOrWhiteSpace(name))
+			{
+				nameOnNext = true;
+			}
+			else if (nameOnNext)
+			{
+				nameOnNext = false;
+				name = content.Trim();
+			}
+			else if (previousLine.Contains("ADVANCED GCE") && string.IsNullOrWhiteSpace(course))
+			{
+				var split = content.Split(" ");
+
+				Console.WriteLine(split);
+				Console.WriteLine(string.Join("", split.Take(split.Length - 2)));
+
+				course = string.Join("", split.Take(split.Length - 2)).Trim();
+
+				grade = split[^1].Trim();
+			}
+			else if (line.Contains("Candidate Number:"))
+			{
+				var centreCan = content.Split(" ")[2].Trim();
+
+				Console.WriteLine(centreCan);
+				
+				centrenum = centreCan.Split("/")[0].Trim();
+				candidate = centreCan.Split("/")[1].Trim();
 			}
 
 			previousLine = content;
